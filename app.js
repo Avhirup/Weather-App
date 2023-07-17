4da3881b2e37f5b00cede8d779167205ccef8f70
Avhi <avhirup0220@gmail.com>
2023-07-18 02:25:47 +0530

    Trying to fix the bug

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, './public')));
 
 let t = "";
@@ -68,46 +67,10 @@ app.post('/', (req, res) => {
     })
 })
 
-app.post('/main', (req, res) => {
-    const cityName = req.body.location;
-    const apiKey = "8f7a8a904243c4467a750bc16cdfb594";
-    const unit = "metric";
-
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${unit}`;
-
-    https.get(url, (response) => {
-        response.on("data", (data) => {
-            const weatherData = JSON.parse(data);
-
-            if (weatherData.cod === "404") {
-                res.render("error");
-            }
-
-            else {
-                const city = weatherData.name;
-                const lon = weatherData.coord.lon;
-                const lat = weatherData.coord.lat;
-                const description = weatherData.weather[0].main;
-                const icon = weatherData.weather[0].icon;
-                const temp = weatherData.main.temp;
-                const maxTemp = weatherData.main.temp_max
-                const minTemp = weatherData.main.temp_min;
-                const humidity = weatherData.main.humidity;
-                const imgURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-
-                res.render("main", { cityName: city, lat: lat, lon: lon, description: description, temp: temp, humidity: humidity, maxTemp: maxTemp, minTemp: minTemp, img: imgURL });
-            }
-        })
-    })
-})
-
 app.post('/error', (req, res) => {
     res.redirect('/');
 })
 
-// app.listen(port, () => {
-//     console.log(`Server has started on port: ${port}`);
-// })
 
 const server = app.listen(process.env.PORT || 4000);
 const portNumber = server.address().port;
